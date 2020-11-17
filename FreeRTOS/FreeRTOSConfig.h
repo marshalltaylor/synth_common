8f93155e1c9e1db798d6336d9df3d4f4881e2014
marshalltaylor <marshall.taylor@sparkfun.com>
2020-11-16 17:05:16 -0700

    Enabled watermark and overflow detector

--- FreeRTOS/FreeRTOSConfig.h
+++ FreeRTOS/FreeRTOSConfig.h
@@ -101,7 +101,7 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 7 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)15360)
+#define configTOTAL_HEAP_SIZE                    ((size_t)32768)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_16_BIT_TICKS                   1 // Set to 1 so that we can observe rollover in a reasonable time
 #define configUSE_MUTEXES                        1
@@ -132,6 +132,9 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil             0
 #define INCLUDE_vTaskDelay                  1
 #define INCLUDE_xTaskGetSchedulerState      1
+
+#define configCHECK_FOR_STACK_OVERFLOW 2
+#define INCLUDE_uxTaskGetStackHighWaterMark 1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
