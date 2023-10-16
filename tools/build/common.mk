2fd4860d88c705dafb2466999ab148d2622f2afd
marshalltaylor <fake@none.com>
2023-10-15 19:29:54 -0600

    Bringing up h7 board

--- tools/build/common.mk
+++ tools/build/common.mk
@@ -21,7 +21,7 @@ TARGET_EXEC := $(BUILD_DIR)/output.elf
 TARGET_BIN := $(BUILD_DIR)/output.bin
 
 # TODO: Best place for linker file?
-LINKER_FILE = $(REPO_ROOT)/bsp/STM32F446RETx_FLASH.ld
+LINKER_FILE = $(REPO_ROOT)/bsp/CubeProject/STM32H743ZITX_FLASH.ld
 
 BUILD_PATHS = \
 $(BUILD_DIR) \
@@ -33,7 +33,8 @@ $(addprefix $(BUILD_DIR)/dep/,$(SOURCE_DIRS))
 # Tools
 #GCC_PATH = /cygdrive/c/STM32/gcc-arm-none-eabi/bin
 #GCC_PATH = C:/STM32/gcc-arm-none-eabi/bin
-GCC_PATH = C:/STM32/gcc_9_2020-q2-update/bin
+#GCC_PATH = C:/STM32/gcc_9_2020-q2-update/bin
+GCC_PATH = C:/STM32/gcc-arm-none-eabi-10.3-2021.10/bin
 CC = $(GCC_PATH)/arm-none-eabi-gcc
 PP = $(GCC_PATH)/arm-none-eabi-g++
 AS = $(GCC_PATH)/arm-none-eabi-as
@@ -49,7 +50,9 @@ GDB = $(GCC_PATH)/arm-none-eabi-gdb
 # c
 #  -DUSE_HAL_DRIVER -DSTM32F7xx  '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -fmessage-length=0 -Og -ffunction-sections
 # -v
-CFLAGS += -DUSE_FULL_LL_DRIVER -DARM_MATH_CM4 -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32F4xx -DSTM32F446xx -gdwarf-4 -g3 -Wall -Werror -c -O0
+
+# WAS FOR 4: CFLAGS += -DUSE_FULL_LL_DRIVER -DARM_MATH_CM4 -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32F4xx -DSTM32F446xx -gdwarf-4 -g3 -Wall -Werror -c -O0
+CFLAGS += -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_FULL_LL_DRIVER -DUSE_HAL_DRIVER -DSTM32H743xx -c -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb
 
 # Generate dependency information
 CFLAGS += -MMD -MP -MF "$(@:$(BUILD_DIR)/obj/%.o=$(BUILD_DIR)/dep/%.d)"
@@ -62,10 +65,10 @@ CFLAGS += -fmax-errors=1
 CPPFLAGS = $(CFLAGS) -std=c++11 -fno-rtti
 
 # assembler
-ASFLAGS = -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -g -v
+ASFLAGS = -mcpu=cortex-m7 -g3 -DDEBUG -c -x assembler-with-cpp --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb 
 
 # linker -lstdc++ -lsupc++  -Wl,--gc-sections  -fno-lto
-LDFLAGS = -v -lc -g3 -lm -lstdc++ -lsupc++ -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -u _printf_float -specs=nosys.specs -specs=nano.specs -T$(LINKER_FILE) -Wl,-Map=$(BUILD_DIR)/output.map -Wl,--gc-sections
+LDFLAGS = -mcpu=cortex-m7 --specs=nosys.specs -static --specs=nano.specs -Wl,--gc-sections -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -lc -lm -T$(LINKER_FILE) -Wl,-Map=$(BUILD_DIR)/output.map -Wl,--gc-sections
 #LDFLAGS = -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -specs=nano.specs -T$(LINKER_FILE) -Wl,-Map=$(BUILD_DIR)/$(TARGET).map,--cref -Wl,--gc-sections
 
 # binaries
@@ -103,7 +106,7 @@ $(BUILD_DIR)/obj/%.o: %.c
 $(BUILD_DIR)/obj/%.o: %.s
 	@echo "#### Compiling: $<"
 	@echo "$(shell cygpath -w -a $<)"
-	$(AS) -c $(ASFLAGS) $(addprefix -I,$(C_INCLUDES)) $< -o $@
+	$(CC) -c $(ASFLAGS) $(addprefix -I,$(C_INCLUDES)) $< -o $@
 	@echo
 
 # Archiver
